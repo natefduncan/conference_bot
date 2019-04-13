35aa34ed9a941be5b90403abde5d52744a39c7c1
Nathan Duncan <natefduncan@gmail.com>
2019-04-13 15:40:07 -0500

    spider

---
+++ conference_bot/spiders/gen_conference_spider.py
@@ -0,0 +1,35 @@
+#General packages
+import scrapy
+import os
+
+path = os.path.dirname(os.path.realpath(__file__))
+os.chdir(path)
+
+#Selenium packages
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+
+class google(scrapy.Spider):
+    name = "gen_conference"
+    
+    def start_requests(self):
+        urls = ["https://scriptures.byu.edu/#::g"]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+    
+    def __init__(self):
+        self.driver = webdriver.Firefox()
+        
+    def parse(self, response):
+        self.driver.get(response.url)
+        
+        response = scrapy.Selector(text=self.driver.page_source)
+        
+        with open("test.txt", "w+") as file: 
+            file.write(response.body)
+        
+        #Get search box input. 
+        #search_box = self.driver.find_element_by_id("searchboxinput")
+        
