1c62e427ac2fc8b95914c070604e2c5145207216
Nathan Duncan <natefduncan@gmail.com>
2019-04-14 22:11:51 -0500

    edits

---
+++ conference_bot.py
@@ -0,0 +1,44 @@
+import os
+from textgenrnn import textgenrnn
+from pathlib import Path
+
+#Directories
+path = Path(os.path.dirname(os.path.realpath(__file__)))
+data_path = path / "conference_bot" / "spiders" / "Data"
+os.chdir(str(data_path))
+os.getcwd()
+#Get text function. 
+def get_text(file_name):
+    with open(file_name, 'r') as file:
+        data = file.read().replace('\n', '')
+    return data
+
+#Create master text file. 
+files = os.listdir(data_path)
+
+text = ""
+for i in files:
+    print(i)
+    text += " " + get_text(i)
+
+os.chdir(str(path))
+
+#Erase the file if it exists.
+config = Path(str(path) + "//all_talks.txt")
+if config.is_file():
+    with open("all_talks.txt", "w+") as file:
+        file.write("")
+ 
+#Write all the talks into one text file. 
+with open("all_talks.txt", "a+") as file:
+    file.write(text)
+
+textgen = textgenrnn()
+textgen.train_from_largetext_file("all_talks.txt", new_model=True, num_epochs=1,
+                                  word_level=True,
+                                  max_length=10,
+                                  max_gen_length=50,
+                                  max_words=5000)
+
+textgen.save('conference_weights.hdf5')
+
